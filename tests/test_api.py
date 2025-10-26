9dc33c5eb79832cccaff7e6310b97da6fdf67f49
Hironsan <hiroki.nakayama.py@gmail.com>
2025-10-26 17:39:17 +0900

    Save the model as ONNX format

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,33 +1,24 @@
 import unittest
-from pprint import pprint
 
 from hatesonar.api import Sonar
 
 
 class TestAPI(unittest.TestCase):
-
     @classmethod
-    def setUpClass(cls):
-        cls.text = 'At least I\'m not a nigger'
+    def setUpClass(cls) -> None:
+        cls.text = "At least I'm not a nigger"
 
-    def test_ping(self):
+    def test_ping(self) -> None:
         sonar = Sonar()
         res = sonar.ping(self.text)
-        pprint(res)
-        self.assertIn('text', res)
-        self.assertIn('top_class', res)
-        self.assertIn('classes', res)
-        self.assertIsInstance(res['text'], str)
-        self.assertIsInstance(res['top_class'], str)
-        self.assertIsInstance(res['classes'], list)
-        for d in res['classes']:
-            self.assertIn('class_name', d)
-            self.assertIn('confidence', d)
-            self.assertIsInstance(d['class_name'], str)
-            self.assertIsInstance(d['confidence'], float)
-
-    def test_get_weight(self):
-        sonar = Sonar()
-        weights = sonar.get_weights(self.text)
-        from pprint import pprint
-        pprint(weights)
+        assert "text" in res
+        assert "top_class" in res
+        assert "classes" in res
+        assert isinstance(res["text"], str)
+        assert isinstance(res["top_class"], str)
+        assert isinstance(res["classes"], list)
+        for d in res["classes"]:
+            assert "class_name" in d
+            assert "confidence" in d
+            assert isinstance(d["class_name"], str)
+            assert isinstance(d["confidence"], float)
